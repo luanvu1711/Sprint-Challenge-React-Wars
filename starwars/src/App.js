fe5513b13bc9969fa04888ff13690365a12d6266
Luan vu <luanvu1711@gmail.com>
2020-03-06 20:07:56 -0500

    components done

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
-
+import axios from 'axios';
+import Posts from "./components/Posts.js";
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -8,10 +9,37 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [people, setPeople] = useState([]);
+  
 
+  useEffect(() => {
+    axios.get("https://swapi.co/api/people/")
+    .then(response => {
+       setPeople(response.data.results);
+      
+    });   
+  }, []);
+  useEffect(() => {
+
+  }, [people]);
+
+  
+  
+
+  
+
+
+  
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+     {
+       people.map(item=> {
+        return (
+          <Posts props = {item}/>
+        )
+       })
+     }
     </div>
   );
 }
---
+++ starwars/src/components/Posts.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Posts = props => {
+    console.log(props);
+   return (
+      
+    <p>{props.props.name}</p>
+   )
+}
+
+export default Posts;
